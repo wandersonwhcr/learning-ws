3599c275778d6e615bbe0153a3343bf731f2a880
Wanderson <wandersonwhcr@gmail.com>
2019-02-27 21:03:30 -0300

    Finish a simple chat

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const http = require('http');
 const morgan = require('morgan');
+const socket = require('socket.io');
 
-const app = express();
+const app    = express();
+const server = http.Server(app);
+const io     = socket(server);
 
 app.use(morgan('tiny'));
 app.use(express.static('public'));
+app.use('/socket.io.js', express.static(__dirname + '/node_modules/socket.io-client/dist/socket.io.js'));
 
-app.listen(8000);
+io.on('connection', (socket) => {
+    // Message Capture!
+    socket.on('message', (message) => {
+        // Broadcasting
+        io.emit('message', message);
+    });
+});
+
+server.listen(8000);
